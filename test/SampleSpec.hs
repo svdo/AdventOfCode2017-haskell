b83103c8ddd8f2976c70f2dedbe81e031bbca140
Stefan van den Oord <soord@mac.com>
2017-12-04 15:27:11 +0100

    Renamed things for more clarity

--- test/SampleSpec.hs
+++ test/SampleSpec.hs
@@ -8,7 +8,9 @@ main =
   describe "Prelude.head" $ do
     it "returns the first element of a list" $
       head [23 ..] `shouldBe` (23 :: Int)
+
     it "returns the first element of an *arbitrary* list" $
       property $ \x xs -> head (x : xs) == (x :: Int)
+
     it "throws an excpetion if used with an empty list" $
       evaluate (head []) `shouldThrow` anyException
