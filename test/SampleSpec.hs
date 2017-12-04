a7d6571c627dafefe68cb0b45ad6fc42aef61c9f
Stefan van den Oord <soord@mac.com>
2017-12-04 15:34:44 +0100

    Enabled hspec-discover

--- test/SampleSpec.hs
+++ test/SampleSpec.hs
@@ -1,16 +1,18 @@
+module SampleSpec (main, spec) where
+
 import Test.Hspec
 import Test.QuickCheck
 import Control.Exception (evaluate)
 
 main :: IO ()
-main =
-  hspec $
+main = hspec spec
+
+spec :: Spec
+spec =
   describe "Prelude.head" $ do
     it "returns the first element of a list" $
       head [23 ..] `shouldBe` (23 :: Int)
-
     it "returns the first element of an *arbitrary* list" $
       property $ \x xs -> head (x : xs) == (x :: Int)
-
     it "throws an excpetion if used with an empty list" $
       evaluate (head []) `shouldThrow` anyException
