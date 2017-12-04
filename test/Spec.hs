0c79ac28ca278de7e6237731fdded2670c58f26d
Stefan van den Oord <soord@mac.com>
2017-12-04 15:00:09 +0100

    Sample test case from Hspec web site

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,14 @@
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main =
+  hspec $
+  describe "Prelude.head" $ do
+    it "returns the first element of a list" $
+      head [23 ..] `shouldBe` (23 :: Int)
+    it "returns the first element of an *arbitrary* list" $
+      property $ \x xs -> head (x : xs) == (x :: Int)
+    it "throws an excpetion if used with an empty list" $
+      evaluate (head []) `shouldThrow` anyException
