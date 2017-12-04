ef84ad13efab6c9422536dba26debc899fa54284
Stefan van den Oord <soord@mac.com>
2017-12-04 17:08:42 +0100

    Clearer test structure

--- test/Day1Spec.hs
+++ test/Day1Spec.hs
@@ -9,18 +9,19 @@ main = hspec spec
 
 spec :: Spec
 spec =
-  describe "parsing" $ do
+  describe "day 1" $ do
     it "converts string to list of ints" $
       parseInput "123" `shouldBe` ([1, 2, 3] :: [Int])
 
-    it "solves captcha 1234" $
-      solveCaptcha "1234" `shouldBe` 0
+    describe "part 1" $ do
+      it "solves captcha 1234" $
+        solveCaptcha "1234" `shouldBe` 0
 
-    it "solves captcha 1122" $
-      solveCaptcha "1122" `shouldBe` 3
+      it "solves captcha 1122" $
+        solveCaptcha "1122" `shouldBe` 3
 
-    it "solves captcha 1111" $
-      solveCaptcha "1111" `shouldBe` 4
+      it "solves captcha 1111" $
+        solveCaptcha "1111" `shouldBe` 4
 
-    it "solves captcha 91212129" $
-      solveCaptcha "91212129" `shouldBe` 9
+      it "solves captcha 91212129" $
+        solveCaptcha "91212129" `shouldBe` 9
