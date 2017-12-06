56a4f243b03de1ae37ea7214db63c8aaf4fb933f
Stefan van den Oord <soord@mac.com>
2017-12-06 08:16:54 +0100

    Day 2 part 2

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,8 @@ main = do
   putStrLn ("  - part 1: " ++ show (solveCaptcha captcha))
   putStrLn ("  - part 2: " ++ show (solveCaptcha2 captcha))
   putStrLn "Day 2:"
+  putStrLn "  - part 1: "
   day2input <- readFile "app/day2.spreadsheet"
   print . checksum $ day2input
-
+  putStrLn "  - part 2: "
+  print . sumOfEvenlyDivisible $ day2input
--- test/Day2Spec.hs
+++ test/Day2Spec.hs
@@ -16,9 +16,28 @@ spec =
     it "can determine a sample checksum" $
       checksum "5\t1\t9\t5\n7\t5\t3\n2\t4\t6\t8" `shouldBe` 18
 
-    it "can parse a spreadsheet string" $
+    it "can parse a spreadsheet string" $ do
+      let sheetString = "790\t99\t345\n302\t463\t59\n899\t962\t77\n"
+      let expected = [[790,99,345],[302,463,59],[899,962,77]]
       parseSpreadsheet sheetString `shouldBe` expected
-        where sheetString = "790\t99\t345\n302\t463\t59\n899\t962\t77\n"
-              expected = [[790,99,345],[302,463,59],[899,962,77]]
 
+    it "can solve part 1" $
+      checksum spreadsheet `shouldBe` 46402
 
+    describe "part 2" $ do
+      it "can generate pairs of elements in a list" $
+        pairs [1,2,3,4] `shouldBe` [(1,2), (1,3), (1,4),
+                                    (2,1), (2,3), (2,4),
+                                    (3,1), (3,2), (3,4),
+                                    (4,1), (4,2), (4,3)]
+
+      it "can compute div of list of pairs" $
+        divs [(2,2), (4,2), (5,2)] `shouldBe` [1,2,0]
+
+      it "can compute answer of sample data" $
+        computeSumOfEvenlyDivisible [[5, 9, 2, 8],
+                                    [9, 4, 7, 3],
+                                    [3, 8, 6, 5]] `shouldBe` 9
+
+      it "can solve part 2" $
+        sumOfEvenlyDivisible spreadsheet `shouldBe` 265
