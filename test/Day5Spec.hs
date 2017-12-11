e8640f0b4c347f1483e7ae27263d2da5e1837f54
Stefan van den Oord <soord@mac.com>
2017-12-11 09:58:45 +0100

    First step: hard-coded the example sequence

---
+++ test/Day5Spec.hs
@@ -0,0 +1,21 @@
+module Day5Spec (main, spec) where
+
+import Test.Hspec
+import Day5
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec =
+  describe "Day 5" $ do
+    it "does one step in simplest case" $
+      stepsToExit [1] `shouldBe` 1
+
+    it "can update the list" $ do
+      update 0 [1, 2, 3] `shouldBe` [2, 2, 3]
+      update 1 [1, 2, 3] `shouldBe` [1, 3, 3]
+      update 2 [1, 2, 3] `shouldBe` [1, 2, 4]
+
+    it "can jump through the example" $
+      jump 0 0 [0,3,0,1,-3] `shouldBe` [5]
