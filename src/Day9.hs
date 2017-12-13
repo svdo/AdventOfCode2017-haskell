351c2ccaa2530cfc46fb9bd5e8faf9ff34a872b3
Stefan van den Oord <soord@mac.com>
2017-12-13 21:05:31 +0100

    It can count the number of groups

--- src/Day9.hs
+++ src/Day9.hs
@@ -1,11 +1,14 @@
 module Day9
   ( stream
+    , garbage
   ) where
+
+--import Debug.Trace
 
 -- Stream -> Group
 -- Group -> '{' Things '}'
--- Things -> Thing | Thing ',' Things
--- Things -> Group | Garbage
+-- Things -> Thing ',' Things | Thing
+-- Thing -> Group | Garbage
 -- Garbage -> '<' AnyCharExceptGtEscaped* '>'
 -- AnythingExceptGtEscaped -> '!' Char | AnythingExceptGt
 -- AnythingExceptGt -> Char \\ '>'
@@ -18,57 +21,38 @@ stream xs
   where (remaining, valid, count) = group xs
 
 group :: String -> (String, Bool, Int)
+--group xs | trace ("group " ++ xs) False = undefined
 group ('{':xs)
   | null xs                 = ([], False, count)
-  | head afterThings == '}' = (tail afterThings, True, count+1)
-  | otherwise               = (afterThings, False, count)
-  where (afterThings, valid, count) = things xs
+  | head endOfGroup == '}'  = (tail endOfGroup, True, count+1)
+  | otherwise               = (endOfGroup, False, count)
+  where (endOfGroup, valid, count) = things xs
+group xs = (xs, False, 0)
 
 things :: String -> (String, Bool, Int)
+--things xs | trace ("things " ++ xs) False = undefined
 things [] = ([], True, 0)
-things ('}':xs) = ('}':xs, True, 0)
-things ('{':xs) = group ('{':xs)
+things xs
+  | head endOfThing == ',' = (endOfMoreThings, moreValid, count+moreCount)
+  | otherwise              = (endOfThing, valid, count)
+  where (endOfThing, valid, count) = thing xs
+        (endOfMoreThings, moreValid, moreCount) = things (tail endOfThing)
 
+thing :: String -> (String, Bool, Int)
+--thing xs | trace ("thing " ++ xs) False = undefined
+thing ('}':xs) = ('}':xs, True, 0)
+thing xs
+  | validGroup = (remaining, validGroup, count)
+  | otherwise  = garbage xs
+  where (remaining, validGroup, count) = group xs
 
+garbage :: String -> (String, Bool, Int)
+garbage ('<':xs)
+  | head endOfGarbage == '>' = (tail endOfGarbage, True, count)
+  where (endOfGarbage, valid, count) = anythingExceptGt xs
 
---  | not valid               = (afterThings, False)
---  | null xs                 = ([], False)
---  | head afterThings == '}' = (tail afterThings, True)
---  | otherwise               = (afterThings, False)
---  where
---    (afterThings, valid) = things xs
---
---things :: String -> (String, Bool)
---things = thing
---
---thing :: String -> (String, Bool)
---thing [] = ([], True)
---thing xs = garbage xs
---
---garbage :: String -> (String, Bool)
---garbage [] = ([], False)
---garbage ('<':xs)
---  | not valid              = (xs, False)
---  | null xs                = ([], False)
---  | head afterChars == '>' = (tail afterChars, True)
---  | otherwise              = (afterChars, False)
---  where (afterChars, valid) = manyCharsExceptGt xs
---
---manyCharsExceptGt :: String -> (String, Bool)
---manyCharsExceptGt (x:xs)
---  | x == '>'  = (x:xs, True)
---  | otherwise = manyCharsExceptGt xs
+anythingExceptGt :: String -> (String, Bool, Int)
+anythingExceptGt ('!':x:xs) = anythingExceptGt xs
+anythingExceptGt ('>':xs) = ('>':xs, True, 0)
+anythingExceptGt (x:xs) = anythingExceptGt xs
 
---import qualified Text.Parsec as Parsec
--- I am the error message infix operator, used later:
---import Text.Parsec ((<?>))
---import Control.Applicative
-
--- alias Parsec.parse for more concise usage in my examples:
---parse rule text = Parsec.parse rule "(source)" text
-
---anythingExceptGt :: Parsec.Parsec String () ()
---anythingExceptGt = Parsec.noneOf ['>']
-
---escape :: Parsec.Parsec String () ()
---escape = Parsec.char '!' >> Parsec.anyChar
--- test/Day9Spec.hs
+++ test/Day9Spec.hs
@@ -18,5 +18,36 @@ spec =
     it "parses nested empty group" $
       stream "{{}}" `shouldBe` ([], True, 2)
 
---    it "parses a group of garbage" $
---      stream "{<asfasjkhfashflas>}" `shouldBe` ([], True)
+    it "parses empty garbage" $
+      stream "{<>}" `shouldBe` ([], True, 1)
+
+    it "recognizes empty garbage" $
+      garbage "<>" `shouldBe` ([], True, 0)
+
+    it "recognizes non-empty garbage" $
+      garbage "<a/b8*>" `shouldBe` ([], True, 0)
+
+    it "parses a group of garbage" $
+      stream "{<asfasjkhfashflas>}" `shouldBe` ([], True, 1)
+
+    it "recognizes garbage with escaped gt" $
+      garbage "<!>>" `shouldBe` ([], True, 0)
+
+    it "recognizes sample garbage" $ do
+      garbage "<<<<>" `shouldBe` ([], True, 0)
+      garbage "<{!>}>" `shouldBe` ([], True, 0)
+      garbage "<!!>" `shouldBe` ([], True, 0)
+      garbage "<!!!>>" `shouldBe` ([], True, 0)
+      garbage "<{o\"i!a,<{i<a>" `shouldBe` ([], True, 0)
+
+    it "parses comma separated things" $
+      stream "{{},{}}" `shouldBe` ([], True, 3)
+
+    it "counts the number of groups in the samples" $ do
+      stream "{{{}}}" `shouldBe` ([], True, 3)
+      stream "{{},{}}" `shouldBe` ([], True, 3)
+      stream "{{{},{},{{}}}}" `shouldBe` ([], True, 6)
+      stream "{<{},{},{{}}>}" `shouldBe` ([], True, 1)
+      stream "{<a>,<a>,<a>,<a>}" `shouldBe` ([], True, 1)
+      stream "{{<a>},{<a>},{<a>},{<a>}}" `shouldBe` ([], True, 5)
+      stream "{{<!>},{<!>},{<!>},{<a>}}" `shouldBe` ([], True, 2)
