3829dcfac002590b4565c51bccac74ee35e23c47
Stefan van den Oord <soord@mac.com>
2017-12-25 11:34:37 +0100

    Day 12 part 1

--- app/Main.hs
+++ app/Main.hs
@@ -11,6 +11,7 @@ import Day8
 import Day9
 import Day10
 import Day11
+import Day12
 
 main :: IO ()
 main = do
@@ -49,3 +50,5 @@ main = do
   putStrLn "Day 11:"
   putStrLn ("  - part 1: " ++ show (distance (parseDirections day11Input)))
   putStrLn ("  - part 2: " ++ show (maxDistance (parseDirections day11Input)))
+  putStrLn "Day 12:"
+  putStrLn ("  - part 1: " ++ show (length (reachable 0 (parseNodes day12Input) [])))
--- test/Day12Spec.hs
+++ test/Day12Spec.hs
@@ -8,6 +8,32 @@ main = hspec spec
 
 spec :: Spec
 spec =
-  describe "Day 12" $
-    it "can test" $
-      3 `shouldBe` 3
+  describe "Day 12" $ do
+    it "parses input line" $
+      parseNode "0 <-> 2, 5, 7" `shouldBe` (0, [2,5,7])
+
+    it "parses multiple lines of input" $
+      parseNodes "0 <-> 2, 5, 7\n2 <-> 0\n" `shouldBe` [(0, [2,5,7]), (2, [0])]
+
+    it "finds a node in a list" $
+      findNode 0 [(0, [2,5,7]), (2, [0])] `shouldBe` (0, [2,5,7])
+
+    it "extracts a node from a list" $
+      extractNode 0 [(0, [2,5,7]), (2, [0])] `shouldBe` ((0, [2,5,7]), [(2, [0])])
+
+    describe "part1" $ do
+      it "handles empty list" $
+        reachable 0 [] [] `shouldBe` []
+
+      it "handles list of one" $
+        reachable 0 [(0, [])] [] `shouldBe` []
+
+      it "includes a reachable node" $
+        reachable 0 [(0, [1]), (1, [0])] [] `shouldBe` [1,0]
+
+      it "includes an indirectly reachable node" $
+        sort (reachable 0 [(0, [1]), (1, [0,2]), (2, [1])] []) `shouldBe` [0,1,2]
+
+      it "solves part 1" $
+        length (reachable 0 (parseNodes day12Input) []) `shouldBe` 128
+
